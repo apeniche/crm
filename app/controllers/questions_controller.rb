610842ff6f55d5464a10763c159af34c0fc6c9ff
Ten Mutunhire <tmutunhire@gmail.com>
2015-03-30 00:05:26 +0000

    manage questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -25,6 +25,7 @@ class QuestionsController < ApplicationController
   # POST /questions.json
   def create
     @question = Question.new(question_params)
+    @question.category = Category.find_by(category_name: params[:question][:category])
 
     respond_to do |format|
       if @question.save
